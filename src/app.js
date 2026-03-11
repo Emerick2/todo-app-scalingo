19384a217742a1849a8501856a43c680777b7c02
Emerick <pacaudemerick@gmail.com>
2026-03-11 10:18:48 +0100

    Ajous de scripts de test pour vérifier le bon fonctionnement du site. Connection des scripts serveur.js et app.js pour qu'ils fonctionnent correctemant

--- src/app.js
+++ src/app.js
@@ -1,27 +1,20 @@
 const express = require("express");
 const cors = require("cors");
-const dotenv = require("dotenv");
 const path = require("path");
 const todoRoutes = require('./routes/todos');
 
-dotenv.config();
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static(path.join(__dirname, "./public")));
 
-app.use(express.static(path.join(__dirname, "public")));
 app.use('/api/todos', todoRoutes);
-
-app.get("/", (req, res) => {
-  res.json({
-    message: "Bienvenue sur la TODO API",
-  });
-});
 
 app.get("/api/health", (req, res) => {
   res.status(200).json({
     status: "OK",
+    success: true,
     timestamp: new Date().toISOString(),
   });
 });
--- src/server.js
+++ src/server.js
@@ -1,37 +1,21 @@
 require('dotenv').config();
-const express = require('express');
-const path = require('path');
-const api = require('./api');
+const app = require('./app');
 const { initDatabase } = require('./models/db');
-const todoRoutes = require('./routes/todos');
 
-const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true })); 
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/api', api);
-app.use('/api/todos', todoRoutes);
-
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-app.get('/index', function (req, res) {
-  res.redirect('/');
-});
-
-initDatabase()
-  .then(() => {
-    app.listen(PORT, () => {
-      console.log(`Base de données prête`);
-      console.log(`Serveur lancé sur : http://localhost:${PORT}`);
+if (process.env.NODE_ENV !== 'test') {
+    initDatabase().then(() => {
+        app.listen(PORT, () => {
+            console.log(`Base de données prête`);
+            console.log(`Serveur lancé sur : http://localhost:${PORT}`);
+        });
+    }).catch(err => {
+        console.error("Erreur fatale au démarrage :", err);
+        process.exit(1);
     });
-  })
-  .catch(err => {
-    console.error("Erreur lors du démarrage :", err);
-    process.exit(1);
-  });
+}
+
+module.exports = { app };
 
 // on y accède en fesant : http://localhost:3000
